d2804d52fef27382eb4e30f50baf3960d5f7133a
PaulaSelim <s.paulakhalil@icloud.com>
2024-09-13 22:47:15 +0300

    added CustomDrawer for navigation and improved UI consistency in ReviewListPage

--- flutter_feedback/lib/views/review_list_page.dart
+++ flutter_feedback/lib/views/review_list_page.dart
@@ -1,8 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_feedback/views/review_input_page.dart';
 import 'package:hive/hive.dart';
 
-class ReviewListPage extends StatefulWidget {
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
   @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: const EdgeInsets.only(top: 100),
+        children: [
+          const ListTile(
+            title: Text(
+              'Menu :',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic, // Make the text italic
+              ),
+            ),
+          ),
+          ListTile(
+            title: const Text('Submit a Review'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const ReviewInputPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('View Reviews'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ReviewListPage()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReviewListPage extends StatefulWidget {
+  const ReviewListPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
   _ReviewListPageState createState() => _ReviewListPageState();
 }
 
@@ -13,8 +61,9 @@ class _ReviewListPageState extends State<ReviewListPage> {
   void _deleteReview(int index) async {
     await _reviewsBox.deleteAt(index);
     setState(() {}); // Update UI after deletion
+    // ignore: use_build_context_synchronously
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Review deleted')),
+      const SnackBar(content: Text('Review deleted')),
     );
   }
 
@@ -22,17 +71,18 @@ class _ReviewListPageState extends State<ReviewListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('All Reviews'),
+        title: const Text('All Reviews'),
       ),
+      drawer: const CustomDrawer(),
       body: FutureBuilder(
         future: _reviewsBox.isOpen ? null : Hive.openBox('reviewsBox'),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           }
 
           if (_reviewsBox.isEmpty) {
-            return Center(child: Text('No reviews yet.'));
+            return const Center(child: Text('No reviews yet.'));
           }
 
           return ListView.builder(
@@ -40,21 +90,25 @@ class _ReviewListPageState extends State<ReviewListPage> {
             itemBuilder: (context, index) {
               final review = _reviewsBox.getAt(index);
               return ListTile(
-                title: Text(review['name']),
+                title: Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: Text(review['name']),
+                ),
                 subtitle: Text(
-                    'Email: ${review['email']}\nReview: ${review['review']}'),
+                  'Email: ${review['email']}\nReview: ${review['review']}',
+                ),
                 trailing: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     IconButton(
-                      icon: Icon(Icons.delete),
+                      icon: const Icon(Icons.delete),
                       onPressed: () {
                         // Confirm before deleting
                         showDialog(
                           context: context,
                           builder: (BuildContext context) {
                             return AlertDialog(
-                              title: Text("Delete Review"),
+                              title: const Text("Delete Review"),
                               content: const Text(
                                   "Are you sure you want to delete this review?"),
                               actions: [
@@ -62,14 +116,14 @@ class _ReviewListPageState extends State<ReviewListPage> {
                                   onPressed: () {
                                     Navigator.of(context).pop();
                                   },
-                                  child: Text("Cancel"),
+                                  child: const Text("Cancel"),
                                 ),
                                 TextButton(
                                   onPressed: () {
                                     Navigator.of(context).pop();
                                     _deleteReview(index);
                                   },
-                                  child: Text("Delete"),
+                                  child: const Text("Delete"),
                                 ),
                               ],
                             );
