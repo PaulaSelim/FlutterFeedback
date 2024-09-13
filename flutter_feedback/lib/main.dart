585e1411979f5e34e8ecda9a9b20cbbbb4ffadd7
PaulaSelim <s.paulakhalil@icloud.com>
2024-09-13 22:47:01 +0300

    added Google Fonts and update theme in ReviewApp

--- flutter_feedback/lib/main.dart
+++ flutter_feedback/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'views/review_input_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,11 +14,34 @@ class ReviewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Review App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+
+        // Define the default brightness and colors.
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          // ···
+          brightness: Brightness.dark,
+        ),
+
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+          // ···
+          titleLarge: GoogleFonts.oswald(
+            fontSize: 30,
+            fontStyle: FontStyle.italic,
+          ),
+          bodyMedium: GoogleFonts.merriweather(),
+          displaySmall: GoogleFonts.pacifico(),
+        ),
       ),
-      home: ReviewInputPage(), // Start with the review input page
+      debugShowCheckedModeBanner: false,
+      home: const ReviewInputPage(), // Start with the review input page
     );
   }
 }
